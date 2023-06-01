1b2f1e42e3686546335a2848f6c8ecb13a36d378
Kunhee Kim <kunheekim@korea.ac.kr>
2023-06-01 22:17:05 +0900

    feat: beacon frame

--- main.h
+++ main.h
@@ -1,13 +1,25 @@
 #include <iostream>
 #include <netinet/in.h>
 #include <pcap.h>
+#include "mac.h"
 using namespace std;
 
 // Source: https://www.radiotap.org/
-struct ieee80211_radiotap_header
+typedef struct ieee80211_radiotap_header
 {
     u_int8_t it_version; /* set to 0 */
     u_int8_t it_pad;
     u_int16_t it_len;     /* entire length */
     u_int32_t it_present; /* fields present */
-} __attribute__((__packed__));
+} __attribute__((__packed__)) radiotapHdr;
+
+typedef struct ieee80211_beacon_frame
+{
+    u_int8_t type;
+    u_int8_t flags;
+    u_int16_t duration;
+    Mac dest;
+    Mac src;
+    Mac bssid;
+    u_int16_t seq;
+} __attribute__((__packed__)) beaconFrame;
